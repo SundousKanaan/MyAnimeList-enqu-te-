e02e6539b538feffd76bb4a0d119e96f23f86719
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-03-30 15:17:53 +0200

    save the form data

--- js/js.js
+++ js/js.js
@@ -1,17 +1,29 @@
-const formInputs = document.querySelectorAll('input[type="text"]');
+let formInputs = document.querySelectorAll('input[type="text"],input[type="number"],input[type="radio"]');
+
 function saveFormInputsToLocal() {
     // Loop over each form input and save its value to local storage
     formInputs.forEach(input => {
-        const inputName = input.name; // Get the name of the input element
-        const inputValue = input.value; // Get the current value of the input element
+        let inputName = input.name; // Get the name of the input element
+        let inputValue = input.value; // Get the current value of the input element
         localStorage.setItem(inputName, inputValue); // Save the input value in local storage
     });
 }
+
+function loadFormInputsFromLocal() {
+    // Loop over each form input and load its value from local storage
+    formInputs.forEach(input => {
+      const inputName = input.name;
+      const storedValue = localStorage.getItem(inputName);
+      if (storedValue) {
+        input.value = storedValue;
+      }
+    });
+  }
 
 // Add event listeners to each input field
 formInputs.forEach(input => {
     input.addEventListener('input', saveFormInputsToLocal);
 });
 
-// Call the function once on page load to save initial values
-saveFormInputsToLocal();
+// Call the function once on page load to save and invul initial values
+loadFormInputsFromLocal();
